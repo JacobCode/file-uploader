5bb8d4a709f7f3c67c6be9a1beebfa0135ab3f96
Jacob Carver <jacxbcarver@gmail.com>
2019-07-26 14:19:51 -0600

    update landing page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 import '../css/home.css';
 
+// Images
 import cloud from '../media/cloud.svg';
+import files from '../media/files.svg';
 
 import Pricing from '../components/Pricing';
 
@@ -11,15 +13,18 @@ export default class Home extends Component {
 		return (
 			<div id="home">
 				{/* Welcome */}
-				<div id="welcome" className="mb-6 container">
+				<section id="welcome" className="mb-6 container">
 					<div className="text">
 						<h1>Welcome To File Uploader</h1>
 						<p>Start uploading images, audio, and other files and be able to access them on other devices</p>
 						<a href="/signin" className="btn btn-primary text-white">Start Now</a>
 					</div>
-				</div>
+					<div className="d-flex align-items-center justify-content-center">
+						<img src={files} alt="Start Uploading Files" />
+					</div>
+				</section>
 				{/* Storage */}
-				<div id="storage" className="d-flex mb-6 container">
+				<section id="storage" className="d-flex mb-6 container">
 					<div className="d-flex flex-column justify-content-center">
 						<h1 className="mb-5">Cloud Storage</h1>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id arcu vitae tortor auctor interdum eu 
@@ -29,9 +34,51 @@ export default class Home extends Component {
 					<div className="d-flex align-items-center justify-content-center">
 						<img src={cloud} alt="Cloud" />
 					</div>
-				</div>
+				</section>
 				{/* Pricing */}
 				<Pricing />
+				{/* Footer */}
+				<footer>
+					<div className="grid">
+
+						<div className="col">
+						<h6>File Uploader</h6>
+							<p>Copyright 2019 &copy;</p>
+						</div>
+						<div className="col">
+							<h6>Customers</h6>
+							<ul>
+								<li>Buyer</li>
+								<li>Supplier</li>
+							</ul>
+						</div>
+						<div className="col">
+							<h6>Company</h6>
+							<ul>
+								<li>About Us</li>
+								<li>Careers</li>
+								<li>Contact Us</li>
+							</ul>
+						</div>
+						<div className="col">
+							<h6>Info</h6>
+							<ul>
+								<li>Terms & Conditions</li>
+								<li>Privacy Policy</li>
+							</ul>
+						</div>
+						<div className="col">
+							<h6>Follow Us</h6>
+							<div className="socials">
+								<i className="fab fa-facebook-f"></i>
+								<i className="fab fa-medium-m"></i>
+								<i className="fab fa-dribbble"></i>
+								<i className="fab fa-twitter"></i>
+								<i className="fab fa-google-plus-g"></i>
+							</div>
+						</div>
+					</div>
+				</footer>
 			</div>
 		)
 	}
